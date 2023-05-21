b25239d4af07d141e4c8438775e1992f9774763b
tintuong <tintuong95@gmail.com>
2023-05-21 16:46:42 +0700

    d

--- next.config.js
+++ next.config.js
@@ -17,19 +17,10 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: 'tk-storage.s3.ap-southeast-1.amazonaws.com',
 			},
-			//   {
-			//     protocol: "http",
-			//     hostname: "localhost",
-			//   },
-			//   {
-			//     protocol: "https",
-			//     hostname: "i.pinimg.com",
-			//   },
-			//   {
-			//     protocol: "https",
-			//     hostname: "media.bizwebmedia.net",
-			//   },
 		],
+	},
+	env: {
+		SERVER: process.env.SERVER,
 	},
 };
 
